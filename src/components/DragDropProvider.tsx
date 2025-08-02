3b8688aa88a32b53a4f27a6b2f5e3c253f2eb870
mattcouri <matt@trinitygroup.com.br>
2025-08-02 16:11:37 -0300

    Updated DragDropProvider.tsx

--- src/components/DragDropProvider.tsx
+++ src/components/DragDropProvider.tsx
@@ -1,9 +1,83 @@
 import React from 'react';
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverEvent,
+  DragStartEvent,
+  DragOverlay,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { restrictToWindowEdges } from '@dnd-kit/modifiers';
 
 interface DragDropProviderProps {
   children: React.ReactNode;
+  onDragEnd?: (event: DragEndEvent) => void;
+  onDragOver?: (event: DragOverEvent) => void;
+  onDragStart?: (event: DragStartEvent) => void;
 }
 
-export default function DragDropProvider({ children }: DragDropProviderProps) {
-  return <>{children}</>;
+export default function DragDropProvider({ 
+  children, 
+  onDragEnd,
+  onDragOver,
+  onDragStart 
+}: DragDropProviderProps) {
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    }),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const handleDragStart = (event: DragStartEvent) => {
+    if (onDragStart) {
+      onDragStart(event);
+    }
+  };
+
+  const handleDragOver = (event: DragOverEvent) => {
+    if (onDragOver) {
+      onDragOver(event);
+    }
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    if (onDragEnd) {
+      onDragEnd(event);
+    }
+  };
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragEnd={handleDragEnd}
+      modifiers={[restrictToWindowEdges]}
+    >
+      {children}
+      <DragOverlay>
+        {/* Drag overlay content will be handled by individual components */}
+      </DragOverlay>
+    </DndContext>
+  );
 }
